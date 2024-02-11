6e196d731a3801f92227e202ed8c73138e3185e6
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-02-11 11:48:31 +0100

    Update index.html and output.css, and add new functionality to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,37 @@
-const generateBtn = document.getElementById('quoteBtn');
-generateBtn.addEventListener('click', displayQuote);
+document.getElementById('NextBtn').addEventListener('click', setQuote);
 
-async function displayQuote() {
-  const quote = await randomQuote();
-  document.querySelector("body > div > div > div:nth-child(4)").style.display = 'none';
-  document.querySelector("body > div > div > h1").innerHTML = quote.content;
-  document.querySelector("body > div > div > p").innerHTML = `- ${quote.author}`;
-  const container = document.querySelector("body > div > div");
-  container.style.backgroundColor = '#fff';
-  container.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
-  container.style.padding = '20px';
-  container.style.borderRadius = '8px';
+async function fetchQuotes() {
+  try {
+    const response = await fetch('quotes.json');
+    const data = await response.json();
+    return data.quotes;
+  } catch (error) {
+    console.error('Error fetching quotes:', error);
+    return [];
+  }
 }
 
-
-async function randomQuote() {
-  const response = await fetch('http://api.quotable.io/random')
-  const quote = await response.json()
-  return quote
+async function getRandomQuote() {
+  const quotes = await fetchQuotes();
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
 }
 
+function setQuote() {
+  getRandomQuote().then(quote => {
+    const quoteText = document.getElementById('quote');
+    quoteText.textContent = quote.quote;
+    const quoteAuthor = document.getElementById('author');
+    quoteAuthor.textContent = `- ${quote.author}`;
+  });
+}
 
-
+document.getElementById('ShareBtn').addEventListener('click', () => {
+  const quote = document.getElementById('quote').textContent;
+  const author = document.getElementById('author').textContent;
+  const message = `${quote} ${author}`;
+  const url = `https://twitter.com/intent/tweet?text=${message
+    .split(' ')
+    .join('%20')}`;
+  window.open(url, '_blank');
+});
